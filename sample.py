6ce3f60a41c87b8da5048ae297f7231128caca29
Rashmitha Poojary <rashmithapoojari3@gmail.com>
2026-05-11 11:56:32 +0530

    First commit

---
+++ sample.py
@@ -0,0 +1,16 @@
+# s=input("Enter s string:")
+# rev=""
+# for i in s:
+#     rev=i+rev
+# print("Reversed string is:",rev)  
+# s=input("Enter a sting:")
+# rev=s[::-1]
+# if s==rev:
+#     print("The atring is palindrome")
+# else:
+#     print("Not a palindrome")         
+class Car:
+    model="benz"
+    price="1200"
+c1=Car()
+print(c1.model)  
